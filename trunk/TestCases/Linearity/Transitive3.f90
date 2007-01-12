11a1d0e3374f82478839bbbb0fbdd7bcd467fa72
ramos <ramos@eac892ec-190f-0410-9fdf-bfd8caa68912>
2007-01-12 18:54:38 +0000

    01/12/07   UseOA-Open64 svn trunk r146 Luis Ramos    - Added new Linearity TestCases to further test Transitivity

---
+++ trunk/TestCases/Linearity/Transitive3.f90
@@ -0,0 +1,23 @@
+! LinearityAnalysis for Automatic Differentiation
+!
+!Result Should be:
+!<<a,a>,linear>, <<a,y>,linear>, <<a,x>,nonlinear>
+!<<y,y>,nonlinear>, <<y,x>,nonlinear>
+!      
+        program main
+          double precision :: a, b, c, x, y, z
+          a = 0.0
+          b = 0.0
+          c = 0.0
+          x = 0.0
+          y = 0.0
+          z = 0.0
+
+          a = 0
+          y = x
+          do i=1,3
+            a = a + y
+            y = y * x
+          enddo
+        end program main
+        
