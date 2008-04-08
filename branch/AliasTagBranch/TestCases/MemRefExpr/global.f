f2908b91f67c9ead3070ad956cd434537fb64c57
malusare <malusare@eac892ec-190f-0410-9fdf-bfd8caa68912>
2008-04-08 03:53:18 +0000

    - Strictly Local and AliasMapXAIF TestCases

--- branch/AliasTagBranch/TestCases/MemRefExpr/global.f
+++ branch/AliasTagBranch/TestCases/MemRefExpr/global.f
@@ -1,46 +1,50 @@
-! =====================================================================
-!                    Testing Global Variables
+! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!
+!                        MemRefExpr TestCase:
+!                        ====================
+!       
+! Features:  Testing MemRefExpr for Global Variable
+!
+!            NamedRef(MemRefType, SymHandle, StrictlyLocal)
+!               MemRefType    => def/use
+!               strictlyLocal => { 0  | NonLocal to the procedure P}
+!                                { 1  | Local to the procedure P}
+!
+! Testing :
+!          [X] MemRefExpr Type (Def/Use)
+!          [X] StrictlyLocal = 0
+!
+! Status : No Issues
 !
 !
-! 1. NamedRef(MemRefType, SymHandle, StrictlyLocal)
-!             MemRefType    => def/use
-!             strictlyLocal => { 0  | NonLocal to the procedure P}
-!                              { 1  | Local to the procedure P}
+! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 !
-! 2. FieldAccess(NamedRef(..., structure_variable,...), field)
-! =================================================================
-
-      module global
-          double precision :: module_a
-
-          ! Global structure
-           type repair_bill
-              real parts(20)
-              real labor
-              real pointer insurance
-          end type repair_bill
-
-      end module
+!                               Analysis:
+!                               =========
+!
+! Procedure foo:
+!      - MemRefHandle(global_c) => NamedRef(def, main::global_c, 0)
+!
+!
+! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!
+!
+!                               Example :
+!                               ==========
 
       program main
-           use global
-           
+
            double precision :: global_c
            common /c/global_c
 
       end program
       
       subroutine foo
-           use global
            common /c/global_c
-      
-           type(repair_bill), target :: first
-           real k
-     
-           module_a = 10      ! NamedRef(def, module_a, 0)
-           
-           global_c=20        ! NamedRef(def, global_c, 0)
 
-           k = first%labor    ! FieldAccess(NamedRef(use,first,1), labor)
-                              ! NamedRef(def, k, 1)
+           global_c=20       
+
       end subroutine
+
+
+
--- branch/AliasTagBranch/TestCases/MemRefExpr/local.f
+++ branch/AliasTagBranch/TestCases/MemRefExpr/local.f
@@ -1,18 +1,24 @@
-! ================================================================
-!                 Testing Local Variables
+! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!
+!                       MemRefExpr Testcase
+!                       ====================
 ! 
+! Features:  Testing Local scalars and pointers, formal scalars
 !
-! There are 2 types of Memory Reference Expressions below:
+!            1. NamedRef(MemRefType, SymHandle, StrictlyLocal)
+!               MemRefType    => def/use
+!               strictlyLocal => { 0  | NonLocal to the procedure P}
+!                                { 1  | Local to the procedure P}
 !
-! 1. NamedRef(MemRefType, SymHandle, StrictlyLocal)
-!             MemRefType    => def/use
-!             strictlyLocal => { 0  | NonLocal to the procedure P}
-!                              { 1  | Local to the procedure P}
 !
-! 2. Deref((MemRefType, SymHandle, StrictlyLocal), numberOfDeref)
-!              numberOfDeref => Number of Dereference Wrappers
+! Testing :
+!          [X] MemRefExpr Type (Def/Use)
+!          [X] StrictlyLocal = 1
 !
-! ================================================================
+! Status : No Issues
+!
+! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
 
       subroutine foo(f)
 
@@ -30,34 +36,40 @@
 
           type(repair_bill), target :: first, second
           
-          ! ======= Program ========
 
-          ! Scalar
-          a=10           ! NamedRef(def, a, 1)
+
+          ! ===== Scalar =====
+
+          a=10           ! NamedRef(def, foo::a, 1)
 
  
-          ! Array 
-          b(1)=1         ! SubSetRef(NamedRef(def, b, 1), )
+          ! ===== Array =====
+
+          b(1)=1         ! SubSetRef(NamedRef(def, foo::b, 1), )
 
 
-          ! Pointer to local
-          p=>a           ! NamedRef(def, p, 1)
-                         ! NamedRef(use, a, 1) 
+          ! ===== Pointer to local =====
 
-          a=p            ! NamedRef(def, a, 1)
-                         ! Deref(NamedRef(use, p, 1), 1)
+          p=>a           ! NamedRef(def, foo::p, 1)
+                         ! NamedRef(use, foo::a, 1) 
+
+          a=p            ! NamedRef(def, foo::a, 1)
+                         ! Deref(NamedRef(use, foo::p, 1), 1)
 
 
-          ! Pointer to formal
-          p=>f           ! NamedRef(def, p, 1)
-                         ! NamedRef(use, f, 1)
+          ! ===== Pointer to formal =====
 
-          f=p            ! NamedRef(def, f, 0)
-                         ! Deref(NamedRef(use, p, 1), 1)
+          p=>f           ! NamedRef(def, foo::p, 1)
+                         ! NamedRef(use, dummy::f, 1)
+
+          f=p            ! NamedRef(def, foo::f, 0)
+                         ! Deref(NamedRef(use, foo::p, 1), 1)
           
 
-         ! Structure variable
-         first=second    ! NamedRef(use, second, 1)
-                         ! NamedRef(def, first, 1)
+         ! ===== Structure variable =====
+
+         first=second    ! NamedRef(use, foo::second, 1)
+                         ! NamedRef(def, foo::first, 1)
+
 
       end subroutine
