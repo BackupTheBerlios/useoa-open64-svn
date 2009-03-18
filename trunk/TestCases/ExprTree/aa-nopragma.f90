56333125e855c6d9435aa6c40906ab9789364501
mstrout <mstrout@eac892ec-190f-0410-9fdf-bfd8caa68912>
2009-03-18 22:04:22 +0000

    Fixed some assertions for Jean. ./test-open64 --oa-ExprTree TestCases/ExprTree/aa-nopragma.B     + is causing mPtr==null assertions     Fixed by making sure we always return an Open64ConstVal for     non integer constants.     + now is causing assertion in OutputExprTreeVisitor.cpp:63,     Looks like we are creating an OpNode for an OPR_CALL, why?     Because of END being an intrinsic.  Made it so we don't create     an OpNode for any intrinsic with 0 children.
    Created new regression tests for the examples Jean sent me.

---
+++ trunk/TestCases/ExprTree/aa-nopragma.f90
@@ -0,0 +1,6 @@
+program allocatedarray
+ double precision  :: x
+ x=2.0
+ x=x
+end program
+
---
+++ trunk/TestCases/ExprTree/aa.f90
@@ -0,0 +1,7 @@
+program allocatedarray
+ double precision  :: x
+!$openad INDEPENDENT(x)
+ x=2.0
+ x=x
+end program
+
